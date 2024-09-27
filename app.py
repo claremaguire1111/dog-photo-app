ff9caed3894b2f3880b578ad824c3551849d9799
Clare_Maguire <clare@thinkblendlabs.com>
2024-09-27 12:14:58 -0500

    Updated design for the logo and header text color

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
 from flask import Flask, render_template, request
+
 import requests
+
 from dog_breeds import prettify_dog_breed
 
 # Initialize the Flask application
 app = Flask(__name__)
 
 def check_breed(breed):
-    return "/".join(breed.split("-"))
+  return "/".join(breed.split("-"))
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET","POST"])
 def dog_image_gallery():
-    errors = []
-    dog_images = []
-    breed = ""
-    
-    if request.method == "POST":
-        breed = request.form.get("breed")
-        if not breed:
-            errors.append("Oops! Please choose a breed.")
-        if breed:
-            response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/30")
-            data = response.json()
-            dog_images = data["message"]
-    
-    # Check if breed is selected, only then prettify it
-    prettified_breed = prettify_dog_breed(breed) if breed else ""
+  errors = []
+  if request.method == "POST":
+    breed = request.form.get("breed")
+    number = request.form.get("number")
+    if not breed:
+      errors.append("Oops! Please choose a breed.")
+    if not number:
+      errors.append("Oops! Please choose a number.")
+    if breed and number:
+      response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/" + number)
+      data = response.json()
+      dog_images = data["message"]
+      return render_template("dogs.html", images=dog_images, breed=prettify_dog_breed(breed), errors=[])
+  return render_template("dogs.html", images=[], breed="", errors=errors)
 
-    return render_template("dogs.html", images=dog_images, breed=prettified_breed, errors=errors)
+
 
 app.debug = True
 
 # Run the flask server
 if __name__ == "__main__":
     app.run()
-
